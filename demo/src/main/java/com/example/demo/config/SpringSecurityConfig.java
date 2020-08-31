4594fb02e12a6635fe8184862ab5f31c07080e2e
QLZL <1480974674@qq.com>
2020-08-31 19:37:32 +0800

    添加权限管理

---
+++ demo/src/main/java/com/example/demo/config/SpringSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.example.demo.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure (HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .logout().permitAll()
+                .and()
+                .formLogin();
+        http.csrf().disable();
+    }
+}
