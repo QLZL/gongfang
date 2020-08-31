407cf4955924e428f7b44ee81a5324aeea3830f3
QLZL <1480974674@qq.com>
2020-08-31 19:47:09 +0800

    权限管理添加web

--- demo/src/main/java/com/example/demo/config/SpringSecurityConfig.java
+++ demo/src/main/java/com/example/demo/config/SpringSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.demo.config;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -15,5 +16,10 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin();
         http.csrf().disable();
+    }
+
+    @Override
+    public void configure (WebSecurity web) throws Exception {
+        web.ignoring().antMatchers();
     }
 }
